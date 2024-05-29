661c2b4f871fc57501933582c410657588266fdc
igortasic123 <63684263+igortasic123@users.noreply.github.com>
2024-05-29 17:23:42 -0400

    push

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,20 +2,27 @@ import { auth } from "@/auth";
 import Navbar from "@/components/navbar/navbar";
 import Spinner from "@/components/spinner";
 import { getNotifications, getProductsByUserId } from "@/lib/server-actions";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 
-type Props = {
+
+
+
+const HomeLayout = async ({
+  children,
+}: Readonly<{
   children: React.ReactNode;
-};
-
-
-const HomeLayout = async ({children }: Props) => {
+}>) => {
   // get the user from the server
 
   const authenticatedUser = await auth();
   const notifications = await getNotifications();
   const products = await getProductsByUserId(authenticatedUser?.user?.id || "");
+
+  if (!authenticatedUser) {
+    redirect("/");
+  }
 
   return (
     <html 
