85b3a0fa123b4a722d6ab963bf9a0d4c4e6de1c9
igortasic123 <63684263+igortasic123@users.noreply.github.com>
2024-06-11 12:37:32 -0400

    commit to main

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from '@vercel/analytics/react';
 
 
 const font = DM_Sans({ subsets: ["latin"] });
@@ -27,6 +28,7 @@ export default function RootLayout({
         
         {children}
         <Toaster />
+        <Analytics />
         
         </body>
     </html>
